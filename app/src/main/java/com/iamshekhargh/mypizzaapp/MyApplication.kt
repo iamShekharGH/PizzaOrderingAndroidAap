de40fea29ac19c9d989307a1a6cce524a6ee4e21
iamShekharGH <shekhargh@shekhargh.com>
2021-05-03 20:05:36 +0530

    initial commit

---
+++ app/src/main/java/com/iamshekhargh/mypizzaapp/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.iamshekhargh.mypizzaapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by <<-- iamShekharGH -->>
+ * on 03 May 2021
+ * at 6:21 PM.
+ */
+@HiltAndroidApp
+class MyApplication : Application()
